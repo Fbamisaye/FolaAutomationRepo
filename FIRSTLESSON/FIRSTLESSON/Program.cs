914636a32827e4fdd24dc91b8b059a7a05b827a5
FOLAKE <fola.bamisaye@gmail.com>
2018-07-26 05:52:22 +0100

    NewSoln

---
+++ FIRSTLESSON/FIRSTLESSON/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FIRSTLESSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boolean - true or false
+            bool QuizAnswer = true;
+
+            //Char - single character
+            char Letter = 'A';
+
+            //Interger - Whole numbers
+            int Number = 5;
+
+            //Decimal - Floating point value/decimal points
+            decimal DecimalNumber = 5.5M;
+
+            //String - strings of characters/words/sentences
+            string Somewords = "Welcome to class";
+
+        }
+    }
+}
