460517d0cddee857dbcbf34e9f36ff3b765b424e
FOLAKE <fola.bamisaye@gmail.com>
2019-04-05 05:34:43 +0100

    Latest

---
+++ TEST/FB2019sol/FolaBami2019Sol/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolaBami2019Sol
+{
+	class Program
+	{
+		//this is single line comment and this is the main method
+		static void Main(string[] args)
+
+		{
+			Console.WriteLine("hello class of 2019"); 
+			
+			/*
+			 * this is a multiline comment
+			 * the following are data types in c#
+			 * the building blocks of codes
+			  */
+
+		// datatype variablesName value:
+
+		int age = 88;
+		char alphabet = 'c';
+		string name = "Andre";
+		double bankbalance = 2500;
+		//DateTime.Today;
+		bool statevalue = true;
+
+			Console.WriteLine("my age is " +  age);
+			Console.WriteLine("the first letter of my name is" + alphabet);
+			Console.WriteLine("my name is " +  name);
+			Console.WriteLine(bankbalance);
+			Console.WriteLine(DateTime.Now);
+			Console.WriteLine("i am a  man "+ statevalue);
+
+			//this is the if statement
+
+		//	if
+				{
+
+			}
+
+			Console.ReadKey();
+			
+		}
+	}
+}
